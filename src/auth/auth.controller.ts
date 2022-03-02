31239f28f707bfa411beff2a217a1cb6bacc7eaa
dipanc1 <dipanchhabra@gmail.com>
2022-03-02 10:38:02 +0530

    pipes and changes dto using dto rather writing email and pasword separately in code

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,23 +1,28 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { Request } from 'express';
+import { Body, Controller, ParseIntPipe, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
 
 @Controller('auth')
-export class AuthController{
-    constructor(private authService: AuthService){}
+export class AuthController {
+  constructor(private authService: AuthService) { }
 
-    @Post('signup') 
-    signup(@Body() dto: AuthDto){
-        console.log({
-            dto
-        });
-        return this.authService.signup();
-    }
-    
+  @Post('signup')
+  signup(
+    @Body('email') email: string,
+    @Body('password', ParseIntPipe) password: string,
+    ) {
+    console.log({
+      email,
+      typeOfEmail: typeof email,
+      password,
+      typeOfPassword: typeof password,
+    });
+    return this.authService.signup();
+  }
+
   @Post('signin')
-    signin(){
-        return this.authService.signin();
-    }
+  signin() {
+    return this.authService.signin();
+  }
 }
 
